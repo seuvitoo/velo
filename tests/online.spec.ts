f93cae5dfcf69bb58592c579e7ddbbaaf0d16d06
Victor Santos <vy.santos@live.com>
2026-05-01 09:32:44 -0300

    Update online.spec.ts

--- tests/online.spec.ts
+++ tests/online.spec.ts
@@ -1,8 +1,9 @@
-import {test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-  test ('webapp deve estar online', async ({ page }) => {
-  await page.goto('http://localhost:5173);
-  
+
+test('webapp deve estar online', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+
   // Expect a title "to contain" a substring.
-  await expect (page). toHaveTitle(/Velô by Papito/);
+  await expect(page).toHaveTitle(/Velô by Papito/);
 })

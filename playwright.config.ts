eb6633947ce44510a6484d74126b700cf2ec2c50
Victor Santos <vy.santos@live.com>
2026-05-01 10:21:33 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, devices } from '@playwright/test';
 
 
 export default defineConfig({
-  timeout: 5000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,

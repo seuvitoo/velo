13f828908488cbc22b2afbd6ba3d56feef066329
Victor Santos <vy.santos@live.com>
2026-05-01 10:50:57 -0300

    Tipos de Busca

--- tests/pedidos.spec.ts
+++ tests/pedidos.spec.ts
@@ -1,17 +1,48 @@
 import { test, expect } from '@playwright/test';
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
 
+  const order = 'VLO-8JY5BZ';
+
+  await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-  await page.getByTestId('search-order-id').fill('VLO-8JY5BZ');
+
+  await page.getByTestId('search-order-id').fill(order);
   await page.getByTestId('search-order-button').click();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-8JY5BZ');
+
+  await expect(page.getByTestId('order-result-id')).toContainText(order);
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 
 
+});
+
+test('deve exibir mensagem quando pedido não for encontrado', async ({ page }) => {
+  const order = 'VLO-123456';
+
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  await page.getByTestId('search-order-id').fill(order);
+  await page.getByTestId('search-order-button').click();
+
+  await expect(page.locator('#root')).toContainText('Pedido não encontrado');
+    await expect(page.locator('#root')).toContainText('Verifique o número do pedido e tente novamente');
+
+    const title = page.getByRole('heading', { name: 'Pedido não encontrado' });
+    await expect(title).toBeVisible();
+
+    const message = page.locator('//p[contains(text(), "Verifique o número do pedido e tente novamente")]');
+    const message2 = page.getByText('Verifique o número do pedido e tente novamente');
+    const message3 = page.getByRole('paragraph', { name: 'Verifique o número do pedido e tente novamente' });
+    const message4 = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' });
+    
+    await expect(message).toBeVisible();
+    await expect(message2).toBeVisible();
+    await expect(message3).toBeVisible();
+    await expect(message4).toBeVisible();
 });
